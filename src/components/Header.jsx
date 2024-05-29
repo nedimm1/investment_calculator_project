0034d42a5839e5de3b8ac62e441220bfb1d23cbd
github_username <nedimmuminovic61@gmail.com>
2024-05-29 15:34:31 +0200

    adding a header

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header(){
+  return <header id="header">
+   <img src="src/assets/investment-calculator-logo.png" alt="website-logo" />
+
+   <h1>Investment Calculator</h1>
+  </header>
+}
